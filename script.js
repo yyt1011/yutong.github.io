56e82f32499a74180ca470e8938a71a6f5f37c6b
yyt1011 <35545773+yyt1011@users.noreply.github.com>
2018-10-21 15:16:23 -0500

    update

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-$('.dnSlide-main').each(function(index,el){
-  var setting={
-    'response':true,
-    afterClickBtnFn:function(i){
-      console.log(i;)
-    }
-  };
-  switch(index){
-    case 0:
-          setting.verticalAlign='top';
-          setting.switching = 'custom';
-          setting.precentWidth='25%';
-          var api=$(el).dnSlide(setting).data('dnSlide');
-          $('.hide').on('click', function(){
-            api.hide(fuction(){
-              alert('hide');
-            }
-          });
-        });
-        break;
-      case 1:
-        setting.autoPlay = true;
-        $(el).dnSlide(setting);
-        break;
-      case 2:
-        setting.verticalAlign = 'bottom';
-        $(el).dnSlide(setting);
-        break;
-      default:
-        $(el).dnSlide(setting);
-        break;
-  }
+$(function(){
+  var galleryImage = $(".gallery").find("img").first();
+  var images = [
+      'image/1.jpg',
+      'image/2.jpg',
+      'image/3_c.jpg',
+      'image/4_c.jpg',
+      'image/5.jpg',
+      'image/6.jpg',
+      'image/7.jpg',
+      'image/8.jpg',
+      'image/9.jpg',
+      'image/10.jpg',
+      'image/11.jpg'
+  ];
+  var i = 0;
+  setInterval(function(){
+    i = (i +1) % images.length;
+    galleryImage.fadeOut(function(){
+      $(this).attr("src", images[i]);
+      $(this).fadeIn();
+    })
+    console.log(galleryImage.attr("src"));
+  }, 2000);
 });
