29e6cedd6a75724087031aa42ed0ad076a86984f
yyt1011 <35545773+yyt1011@users.noreply.github.com>
2018-11-14 12:42:12 -0600

    updatepictures

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ $(function(){
       'image/1.jpg',
       'image/2.jpg',
       'image/3.jpg',
-      'image/4.jpg',
+      'image/4c.jpg',
       'image/5.jpg',
       'image/6.jpg',
       'image/7.jpg',
       'image/8.jpg',
       'image/9.jpg',
-      'image/10.png',
       'image/11.jpg',
+      'image/12.jpg',
   ];
   var i = 0;
   setInterval(function(){
