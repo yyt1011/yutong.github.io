a665427426db455e2cf27e95a05ec1b44abe702c
yyt1011 <35545773+yyt1011@users.noreply.github.com>
2018-12-01 16:01:28 -0600

    pictures

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 $(function(){
-  $(".work_img").find("img").hover(
+  var galleryItems = $(".work_img").find("img");
+  galleryItems.hover(
     function(){
       $(this).stop().css("opacity","0.5");
     },function(){
       $(this).stop().css("opacity","1");
   });
-
-
+  galleryItems.click(function(){
+    var source = $(this).attr('src');
+    var newImage = $('<img>').attr('src', source).css('width','100%');
+    $('.work').stop().css('display','none');
+    $('.second_chart_text').stop().css('display','none');
+    $('.lightbox').empty().append(newImage).fadeIn();
+    $('.lightbox').css('width','90%').css('margin','auto');
+  });
+  $('.lightbox').click(function(){
+    $(this).stop().fadeOut();
+    $('.work').stop().css('display','block');
+    $('.second_chart_text').stop().css('display','block');
+  });
 });
