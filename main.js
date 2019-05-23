21a87efbf6f1464ae0265f7033879ab2fc38ffe5
yyt1011 <35545773+yyt1011@users.noreply.github.com>
2019-05-23 14:24:33 -0400

    fixed interactive
    fixed the interactive part of the charts page

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ $(function(){
   galleryItems.click(function(){
     var source = $(this).attr('src');
     var newImage = $('<img>').attr('src', source).css('width','100%');
+    $('.interactive').stop().css('display','none');
     $('.work').stop().css('display','none');
     $('.second_chart_text').stop().css('display','none');
-    $('.lightbox').empty().append(newImage).fadeIn();
+    $('.lightbox').empty().append(newImage).fadeIn('fast','linear');
     $('.lightbox').css('width','90%').css('margin','auto');
   });
   $('.lightbox').click(function(){
-    $(this).stop().fadeOut();
+    $(this).stop().fadeOut('fast','linear');
+    $('.interactive').stop().css('display','block');
     $('.work').stop().css('display','block');
     $('.second_chart_text').stop().css('display','block');
   });
